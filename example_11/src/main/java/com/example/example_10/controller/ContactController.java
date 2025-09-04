90618ccc6d48db381aa608699143ae53d95b7de9
DR DARKER <oct2001nitin@gmail.com>
2025-09-04 19:19:34 +0530

    validation

--- example_11/src/main/java/com/example/example_10/controller/ContactController.java
+++ example_11/src/main/java/com/example/example_10/controller/ContactController.java
@@ -2,8 +2,12 @@ package com.example.example_10.controller;
 
 import com.example.example_10.model.Contact;
 import com.example.example_10.services.ContactService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,7 +23,8 @@ public class ContactController {
     }
 
     @RequestMapping("/contact")
-    public String contact() {
+    public String contact(Model model) {
+        model.addAttribute("contact", new Contact()); // this is needed for validation to work
         return "contact.html"; // This will render the contact.html template
     }
 
@@ -45,12 +50,15 @@ public class ContactController {
 
 
     @RequestMapping(value="/saveMsg", method = POST)
-    public ModelAndView saveMessage(Contact contact) {
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors error) {
         // Here, you would typically save the message to a database or send an email
         // For this example, we'll just print it to the console
-
+        if(error.hasErrors()){
+            System.out.println("Validation errors occurred");
+            return "contact.html";
+        }
         contactService.saveMessageDetails(contact);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
 
     }
 }
