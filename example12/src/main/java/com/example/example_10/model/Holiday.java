0988e0d2c962405e34e967c402964437a320e3e2
DR DARKER <oct2001nitin@gmail.com>
2025-09-05 19:07:18 +0530

    web scopes

---
+++ example12/src/main/java/com/example/example_10/model/Holiday.java
@@ -0,0 +1,19 @@
+package com.example.example_10.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+public class Holiday {
+    private final String day;
+    private final String reason;
+    private final Type type;
+
+
+    public enum Type {
+        FESTIVAL, FEDERAL
+    }
+
+
+
+}
